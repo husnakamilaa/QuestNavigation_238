92516606bdcc8ba4458fb7f0ba90d069c074e540
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-06 13:52:40 +0700

    feat: menambahkan garis merah menggunakan horizontal divider

--- app/src/main/java/com/example/navigasisederhana/view/Formulir.kt
+++ app/src/main/java/com/example/navigasisederhana/view/Formulir.kt
@@ -6,7 +6,9 @@
     import androidx.compose.foundation.layout.Column
     import androidx.compose.foundation.layout.padding
     import androidx.compose.foundation.layout.width
+    import androidx.compose.material3.DividerDefaults.Thickness
     import androidx.compose.material3.ExperimentalMaterial3Api
+    import androidx.compose.material3.HorizontalDivider
     import androidx.compose.material3.OutlinedTextField
     import androidx.compose.material3.Scaffold
     import androidx.compose.material3.Text
@@ -48,6 +50,11 @@
                     label = {Text(text = "Nama Lengkap")},
                     onValueChange = {},
                 )
+                HorizontalDivider(modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                    thickness = Thickness,
+                    color = Color.Red)
             }
 
         }
