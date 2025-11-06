4517f502e70e6c4cce54682bd045cdd8f45439d9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-06 14:41:19 +0700

    feat: mendeklarasikan layout Column untuk isiRuang atau body scaffold dan menambahkan layout Column lagi didalamnya

--- app/src/main/java/com/example/navigasisederhana/view/TampilData.kt
+++ app/src/main/java/com/example/navigasisederhana/view/TampilData.kt
@@ -1,5 +1,8 @@
 package com.example.navigasisederhana.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -9,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.navigasisederhana.R
 
@@ -28,5 +32,14 @@ fun TampilData(
                 title = { Text(text = stringResource(R.string.tampil), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(R.color.teal_700))
             )
-        }) {  }
+        }) { isiRuang->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))) {
+            items.forEach { items ->
+                Column {
+
+                }
+            }
+        }
+    }
 }
