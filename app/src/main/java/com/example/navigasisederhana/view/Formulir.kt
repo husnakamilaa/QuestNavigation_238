4627a063ecff05ac1220d7a7146b6dfe0ca19337
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-06 13:19:50 +0700

    feat: membuat function FormIsian dan mendeklaraskan parameter untuk menyimpan nilai jenisK dan click button

--- app/src/main/java/com/example/navigasisederhana/view/Formulir.kt
+++ app/src/main/java/com/example/navigasisederhana/view/Formulir.kt
@@ -1,2 +1,13 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navigasisederhana.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
+
+}
