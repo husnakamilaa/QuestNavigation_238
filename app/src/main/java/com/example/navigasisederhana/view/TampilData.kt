f7e348898a22420cb685a9776ac7713692dbe030
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-06 14:30:29 +0700

    feat: mendeklarasikan function TampilData dan mendefinisikan variable static untuk nama, jenis kelamin, dan alamat sebagai dummy data

--- app/src/main/java/com/example/navigasisederhana/view/TampilData.kt
+++ app/src/main/java/com/example/navigasisederhana/view/TampilData.kt
@@ -2,6 +2,17 @@ package com.example.navigasisederhana.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.navigasisederhana.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun TampilData(
+    onBackBtnClick:()-> Unit
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(R.string.alamat), "Yogyakarta")
+    )
+}
